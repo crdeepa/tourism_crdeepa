0093902c5c08a3135c04cdb81cea77bcccc9061f
Deepa CR <45058052+crdeepa@users.noreply.github.com>
2025-11-30 19:46:53 +0530

    Update app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download and load the trained model
-model_path = hf_hub_download(repo_id="crdeepa/tourism-package-prediction/tourism_package_model", filename="best_tourism_package_model_v1.joblib")
+model_path = hf_hub_download(repo_id="crdeepa/tourism_package_model", filename="best_tourism_package_model_v1.joblib")
 model = joblib.load(model_path)
 
 # Streamlit UI
